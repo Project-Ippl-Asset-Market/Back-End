44a1ca2d37749be34f3137f8adea4c5c3eacd5ae
11211016@student.itk.ac.id <11211016@student.itk.ac.id>
2025-01-02 08:49:05 +0800

    Update BE

--- Back-End-main/servis-api-crud-firebase/app.js
+++ Back-End-main/servis-api-crud-firebase/app.js
@@ -6,6 +6,7 @@ import userRoutes from "./routes/userRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
 import { checkRole } from "./middleware/authMiddleware.js";
 import createTransactionController from "./routes/transactionRoutes.js";
+import saveBuyAssetsController from "./routes/transactionRoutes.js";
 import { checkEmail } from "./controllers/authController.js";
 import authRoutes from "./routes/authRoutes.js";
 import assetRoutes from "./routes/assetRoutes.js";
@@ -16,11 +17,11 @@ import myAssetRoutes from "./routes/myAssetRoutes.js";
 import moveAsset from "./routes/moveRoutes.js";
 import revenueRoutes from "./routes/revenueRoutes.js";
 import removeCart from "./routes/removeCartRoutes.js"
+import fetch from "node-fetch";
 
 const app = express();
 const port = 3000;
 
-// Menangani kesalahan (opsional, untuk penanganan kesalahan yang lebih baik)
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.setHeader(
@@ -32,12 +33,18 @@ app.use((err, req, res, next) => {
 });
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://my-asset.vercel.app/",
+    "https://my-asset.vercel.app"
+  ]
+}));
 
-// Gunakan rute
 app.use("/api/users", userRoutes);
 app.use("/api/admins", adminRoutes);
 app.use("/api", createTransactionController);
+app.use("/api", saveBuyAssetsController);
 app.use("/api/users", authRoutes);
 app.use("/api/assets", assetRoutes);
 app.use("/api/carts", cartRoutes);
@@ -49,13 +56,41 @@ app.use("/api", removeCart);
 app.use("/api", revenueRoutes);
 app.use("/api", myAssetRoutes);
 
-// Menangani kesalahan (opsional, untuk penanganan kesalahan yang lebih baik)
+
+app.get("/", (req, res) => {
+  res.send("Welcome Back End Asset Market IPPL!");
+});
+
+
+app.get("/api/proxy-file", async (req, res) => {
+  const fileUrl = req.query.url;
+
+  try {
+    if (!fileUrl) {
+      return res.status(400).send("File URL is required.");
+    }
+
+    const response = await fetch(fileUrl);
+    if (!response.ok) {
+      return res.status(response.status).send(`Failed to fetch file: ${response.statusText}`);
+    }
+
+    res.set("Content-Type", response.headers.get("content-type"));
+    response.body.pipe(res);
+
+  } catch (error) {
+    console.error("Error proxying file:", error);
+    res.status(500).send("Internal Server Error.");
+  }
+});
+
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something went wrong!");
 });
 
-// Start server
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
